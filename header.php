b19608997608fba9e72e8de183cc7424df2a12a6
Denuwan Kawshalya <149412984+CodebyDenu@users.noreply.github.com>
2025-02-11 11:41:36 +0530

    navbarFinal

--- header.php
+++ header.php
@@ -26,14 +26,14 @@
         <div class="th-mobile-menu">
             <ul>
                 <li class="menu-item-has-children">
-                    <a href="home-seo-agency.html">Home</a>
-                    <ul class="sub-menu">
+                    <a href="index.php">Home</a>
+                    <!-- <ul class="sub-menu">
                         <li><a href="home-seo-agency.html">Home SEO Agency</a></li>
                         <li><a href="home-digital-agency.html">Home Digital Agency</a></li>
                         <li><a href="index.php">Home SEO Analysis Agency</a></li>
                         <li><a href="home-seo-marketing.html">Home SEO Marketing</a></li>
 
-                    </ul>
+                    </ul> -->
                 </li>
                 <li><a href="about.html">About Us</a></li>
                 <li class="menu-item-has-children">
@@ -78,7 +78,7 @@
         </div>
     </div>
 </div><!--==============================
-	Header Area
+    Header Area
 ==============================-->
 <header class="th-header header-layout3 header-absolute">
     <div class="header-top header-top-color py-2">
@@ -87,12 +87,14 @@
                 <div class="col-auto d-none d-md-block">
                     <div class="header-links">
                         <ul>
-                            <li class="d-none d-xl-inline-block text-white"><i class="fa-solid fa-location-dot text-white"></i>
+                            <li class="d-none d-xl-inline-block text-white"><i
+                                    class="fa-solid fa-location-dot text-white"></i>
                                 523 Jerry Mench Dr.USA</li>
-                            <li><i class="fa-solid fa-phone text-white"></i><a href="tel:+163217322978" class="text-white">+163 2173 229788</a>
+                            <li><i class="fa-solid fa-phone text-white"></i><a href="tel:+163217322978"
+                                    class="text-white">+163 2173 229788</a>
                             </li>
-                            <li><i class="fa-solid fa-envelope text-white"></i><a
-                                    href="mailto:saorhelp@gmail.com" class="text-white">saorhelp@gmail.com</a>
+                            <li><i class="fa-solid fa-envelope text-white"></i><a href="mailto:saorhelp@gmail.com"
+                                    class="text-white">saorhelp@gmail.com</a>
                             </li>
                         </ul>
                     </div>
@@ -101,10 +103,14 @@
                     <div class="header-right">
                         <p class="title text-white">Open Hours: 09am - 05pm Mon-Sat</p>
                         <div class="social-links">
-                            <a href="https://www.facebook.com/" class="text-white"><i class="fab fa-facebook-f text-white"></i></a>
-                            <a href="https://www.linkedin.com/" class="text-white"><i class="fab fa-linkedin-in text-white"></i></a>
-                            <a href="https://www.twitter.com/" class="text-white"><i class="fab fa-twitter text-white"></i></a>
-                            <a href="https://www.youtube.com/" class="text-white"><i class="fab fa-youtube text-white"></i></a>
+                            <a href="https://www.facebook.com/" class="text-white"><i
+                                    class="fab fa-facebook-f text-white"></i></a>
+                            <a href="https://www.linkedin.com/" class="text-white"><i
+                                    class="fab fa-linkedin-in text-white"></i></a>
+                            <a href="https://www.twitter.com/" class="text-white"><i
+                                    class="fab fa-twitter text-white"></i></a>
+                            <a href="https://www.youtube.com/" class="text-white"><i
+                                    class="fab fa-youtube text-white"></i></a>
                         </div>
                     </div>
                 </div>
@@ -118,70 +124,104 @@
                 <div class="row align-items-center justify-content-between">
                     <div class="col-auto">
                         <div class="header-logo">
-                            <a href="index.php"><img src="assets/img/logo-white2.svg" alt="cody zea" class="text-b fw-semibold"></a>
+                            <a href="index.php"><img src="assets/img/logo.svg" alt="cody zea"
+                                    class="text-b fw-semibold"></a>
                         </div>
                     </div>
                     <div class="col-auto">
                         <nav class="main-menu d-none d-lg-inline-block">
                             <ul>
-                                <li class="menu-item-has-children text-b fw-semibold">
-                                    <a href="home-seo-agency.html" class="text-b fw-semibold">Home</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="home-seo-agency.html" class="text-b fw-semibold">Home SEO Agency</a></li>
-                                        <li><a href="home-digital-agency.html" class="text-b fw-semibold">Home Digital Agency</a></li>
-                                        <li><a href="index.php" class="text-b fw-semibold">Home SEO Analysis Agency</a></li>
-                                        <li><a href="home-seo-marketing.html" class="text-b fw-semibold">Home SEO Marketing</a></li>
+                                <li><a href="index.php" class="text-b fw-semibold">Home</a></li>
+                                <li><a href="about.html" class="text-b fw-semibold">Pricing</a></li>
+                                <li class="menu-item-has-children">
+                                    <a href="#" class="text-b fw-semibold">Offers</a>
+                                    <ul class="mega-menu" style="width: 1200px;">
+                                        <!-- <li><a href="service.html" class="text-b fw-semibold">Service</a></li> -->
+                                        <div class="row">
+                                            <div class="col-12 col-lg-6 d-flex">
+                                                <div class="choose-feature-wrap">
+                                                    <div class="choose-feature cody-bg p-3 rounded-20">
+                                                        <div class="box-icon">
+                                                            <img src="assets/img/icon/choose_feature_1_1.svg"
+                                                                alt="Icon">
+                                                        </div>
+                                                        <div class="media-body">
+                                                            <h3 class="cody-text fs-5">Registered in New Zealand & Sri Lanka
+                                                            </h3>
+                                                            <p class="cody-text">We operate as a fully registered company
+                                                                in both countries, ensuring credibility and trust in
+                                                                every project.</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </ul>
                                 </li>
-                                <li><a href="about.html" class="text-b fw-semibold">About Us</a></li>
-                                <li class="menu-item-has-children text-b fw-semibold">
-                                    <a href="#" class="text-b fw-semibold">Portfolio</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="portfolio.html" class="text-b fw-semibold">Portfolio</a></li>
-                                        <li><a href="portfolio-details.html" class="text-b fw-semibold">Portfolio Details</a></li>
+                                <li class="menu-item-has-children">
+                                    <a href="#" class="text-b fw-semibold">Solutions</a>
+                                    <ul class="mega-menu" style="width: 1200px;">
+                                        <!-- <li><a href="service.html" class="text-b fw-semibold">Service</a></li> -->
+                                        <div class="row">
+                                            <div class="col-12 col-lg-6 d-flex">
+                                                <div class="choose-feature-wrap">
+                                                    <div class="choose-feature cody-bg p-3 rounded-20">
+                                                        <div class="box-icon">
+                                                            <img src="assets/img/icon/choose_feature_1_1.svg"
+                                                                alt="Icon">
+                                                        </div>
+                                                        <div class="media-body">
+                                                            <h3 class="cody-text fs-5">Registered in New Zealand & Sri Lanka
+                                                            </h3>
+                                                            <p class="cody-text">We operate as a fully registered company
+                                                                in both countries, ensuring credibility and trust in
+                                                                every project.</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </ul>
                                 </li>
-                                <li class="menu-item-has-children text-b fw-semibold">
-                                    <a href="#" class="text-b fw-semibold">Pages</a>
-                                    <ul class="sub-menu">
-                                        <li class="menu-item-has-children text-b fw-semibold">
-                                            <a href="#" class="text-b fw-semibold">Shop</a>
-                                            <ul class="sub-menu">
-                                                <li><a href="shop.html" class="text-b fw-semibold">Shop</a></li>
-                                                <li><a href="shop-details.html" class="text-b fw-semibold">Shop Details</a></li>
-                                                <li><a href="cart.html" class="text-b fw-semibold">Cart Page</a></li>
-                                                <li><a href="checkout.html" class="text-b fw-semibold">Checkout</a></li>
-                                                <li><a href="wishlist.html" class="text-b fw-semibold">Wishlist</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="service.html" class="text-b fw-semibold">Service</a></li>
-                                        <li><a href="service-details.html" class="text-b fw-semibold">Service Details</a></li>
-                                        <li><a href="team.html" class="text-b fw-semibold">Team</a></li>
-                                        <li><a href="team-details.html" class="text-b fw-semibold">Team Details</a></li>
-                                        <li><a href="pricing.html" class="text-b fw-semibold">Price Table</a></li>
-                                        <li><a href="testimonial.html" class="text-b fw-semibold">Testimonial</a></li>
-                                        <li><a href="faq.html" class="text-b fw-semibold">FAQ</a></li>
-                                        <li><a href="error.html" class="text-b fw-semibold">Error Page</a></li>
-                                    </ul>
-                                </li>
-                                <li class="menu-item-has-children text-b fw-semibold">
-                                    <a href="#" class="text-b fw-semibold">Blog</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="blog.html" class="text-b fw-semibold">Blog</a></li>
-                                        <li><a href="blog-details.html" class="text-b fw-semibold">Blog Details</a></li>
+                                <li class="menu-item-has-children">
+                                    <a href="#" class="text-b fw-semibold">Blogs</a>
+                                    <ul class="mega-menu" style="width: 1200px;">
+                                        <!-- <li><a href="service.html" class="text-b fw-semibold">Service</a></li> -->
+                                        <div class="row">
+                                            <div class="col-12 col-lg-6 d-flex">
+                                                <div class="choose-feature-wrap">
+                                                    <div class="choose-feature cody-bg p-3 rounded-20">
+                                                        <div class="box-icon">
+                                                            <img src="assets/img/icon/choose_feature_1_1.svg"
+                                                                alt="Icon">
+                                                        </div>
+                                                        <div class="media-body">
+                                                            <h3 class="cody-text fs-5">Registered in New Zealand & Sri Lanka
+                                                            </h3>
+                                                            <p class="cody-text">We operate as a fully registered company
+                                                                in both countries, ensuring credibility and trust in
+                                                                every project.</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </ul>
                                 </li>
                                 <li><a href="contact.html" class="text-b fw-semibold">Contact Us</a></li>
                             </ul>
                         </nav>
                         <div class="header-button">
-                            <button type="button" class="th-menu-toggle d-inline-block d-lg-none"><i class="far fa-bars text-b fw-semibold"></i></button>
+                            <button type="button" class="th-menu-toggle d-inline-block d-lg-none"><i
+                                    class="far fa-bars text-b fw-semibold"></i></button>
                         </div>
                     </div>
                     <div class="col-auto d-none d-lg-block">
                         <div class="header-button">
-                            <button type="button" class="icon-btn th-radius searchBoxToggler"><i class="far fa-search text-b fw-semibold"></i></button>
-                            <a href="contact.html" class="th-btn style1 th-radius th-icon">Get A Quote <i class="fa-regular fa-arrow-right ms-2"></i></a>
+                            <button type="button" class="icon-btn th-radius searchBoxToggler"><i
+                                    class="far fa-search text-b fw-semibold"></i></button>
+                            <a href="contact.html" class="th-btn style1 th-radius th-icon">Get A Quote <i
+                                    class="fa-regular fa-arrow-right ms-2"></i></a>
                         </div>
                     </div>
                 </div>
